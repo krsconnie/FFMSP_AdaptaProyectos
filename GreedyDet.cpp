c2c384915ba39da4b959f38218bd8633aee80c44
Constanza Cristinich <109244120+krsconnie@users.noreply.github.com>
2024-09-08 22:21:35 -0300

    contador minima ocurrencia lista

--- GreedyDet.cpp
+++ GreedyDet.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <string>
 #include <list>
+#include <cstring>
 #include <chrono>
 #include <algorithm>
 #include "getInput.h"
@@ -20,11 +21,11 @@ int main(int argc, char const *argv[]){
     std::string nombreArchivo = argv[2];
     std::list<std::string> lista;
 
-    getStrings(nombreArchivo,lista);
+    getStrings(nombreArchivo,lista); // Llama a la funcion de la librería getInput.h
 
-    int n = std::stoi(nombreArchivo.substr(0,3)); // Cantidad original de strings
+    int n = std::stoi(nombreArchivo.substr(0,3)); // Cantidad original de strings 
     int m = std::stoi(nombreArchivo.substr(4,3)); // Largo del string 
-    std::string solucion[m]; // String solucion 
+    char solucion[m]; // String solucion 
 
 
     std::list<std::string> conjuntoSolucion; 
@@ -33,15 +34,17 @@ int main(int argc, char const *argv[]){
     int contador[4] = {0,0,0,0}; //Arreglo contador inicializado en 0
 
     
+    // Flags
+    std::cout << "Probando... lista con   " << n << "  strings...  de   " << m << "  characters cada una..." << std::endl;
+    std::cout << " Archivo: " << nombreArchivo << "....."  << std::endl;
 
-    std::cout << "Probando... lista con   " << n << "  strings...  de   " << m << "characters cada una..." << std::endl;
-    std::cout << "Archivo:" << nombreArchivo << "....."  << std::endl;
-
-    std::list<std::string>::iterator it = lista.begin();
+   
     // Iterador para recorrer los caracteres del string
      for (int i = 0; i < m; i++){
+        
         // Iterador para recorrer la lista de strings
-        for( std::list<std::string>::iterator it = lista.begin(); it != lista.end();it++){
+        for(std::list<std::string>::iterator it = lista.begin(); it != lista.end();it++){
+        
             char a = (*it).at(i);
 
             // Switch case para contar las ocurrencias de la n posicion de los strings
@@ -65,9 +68,40 @@ int main(int argc, char const *argv[]){
                  default:
                     break;
             }
-        // Comparador de la letra con la menor ocurrencia para agregarla al string solucion
+
         }
+        // Comparador de la letra con la menor ocurrencia para agregarla al string solucion
+        int minIndex = std::distance(std::begin(contador), std::min_element(std::begin(contador), std::end(contador)));
+        std::cout << minIndex << std::endl;
+            
+            switch(minIndex){
+                
+                case 0:
+                    solucion[i]='A';
+                    break;
+
+                case 1:
+                    solucion[i]='C';
+                    break;
+
+                case 2:
+                    solucion[i]='G';
+                    break;
+
+                case 3:
+                    solucion[i]='T';
+                    break;
+
+                default: 
+                    break;
+        }    
+
+        memset(contador, 0, sizeof(contador));
+
+
+
     }
+
    
     return 0;
 };
