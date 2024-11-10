73c7911e848b62508acad6bc389be9fee565dbc8
Tyckong <tocontreras2021@udec.cl>
2024-11-10 05:22:27 -0300

    Begun working on BRKGA function.

--- TerceraEntrega/encoder.cpp
+++ TerceraEntrega/encoder.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 #include <string>
 #include <random>
@@ -10,7 +11,7 @@ vector<double> encoder(const string& input){
     vector<double> encrypted;
     mt19937 generator(static_cast<unsigned>(time(nullptr))); // Random number generator with seed
 
-    for (char ch : input){
+    for(char ch : input){
         if(ch == 'A'){
             uniform_real_distribution<double> distA(0.0, 0.25);
             encrypted.push_back(distA(generator));
@@ -35,10 +36,10 @@ vector<double> encoder(const string& input){
     return encrypted;
 }
 
-string decoder(const vector<double>& encrypted) {
+string decoder(const vector<double>& encrypted){
     string decrypted;
 
-    for (double value : encrypted) {
+    for(double value : encrypted){
         if (value >= 0.0 && value <= 0.25) {
             decrypted += 'A';
         } else if (value > 0.25 && value <= 0.50) {
@@ -55,21 +56,42 @@ string decoder(const vector<double>& encrypted) {
     return decrypted;
 }
 
-/*
-int main() {
-    string input;
-    cin >> input;
+vector<double> BRKGA(vector<double> parentA, vector<double> parentB){
+    vector<double> child;
+    mt19937 generator(static_cast<unsigned>(time(nullptr)));
+    uniform_int_distribution<int> distribution(0, 1);
 
-    vector<double> encoded = encoder(input);
-    cout << "Encrypted values: ";
-    for (double val : encoded) {
-        cout << val << " ";
+    int minlen = min(parentA.size(), parentB.size());
+    for(int i=0; i<minlen; i++){
+        if(distribution(generator) == 0){
+            child.push_back(parentA[i]);
+        }else{
+            child.push_back(parentB[i]);
+        }
+    }
+    return child;
+}
+
+int main() {
+    string parentA, parentB, child;
+    cin >> parentA;
+    cin >> parentB;
+
+    vector<double> encoded1 = encoder(parentA);
+    vector<double> encoded2 = encoder(parentB);
+    cout << "Valores encriptados: " << endl;
+    for(int i=0; i<parentA.size(); i++){
+        cout << i << " => " << "Padre A: " << encoded1[i] << " || Padre B: " << encoded2[i] << endl;
     }
     cout << endl;
 
-    string decoded = decoder(encoded);
-    cout << "Decrypted string: " << decoded << endl;
+    vector<double> encoded_child = BRKGA(encoded1, encoded2);
+
+    string decoded = decoder(encoded_child);
+    cout << "Padre A: " << parentA << endl;
+    cout << "Padre B: " << parentB << endl;
+    cout << "Hijo desencriptado: " << decoded << endl;
 
     return 0;
 }
-*/
+
